1ccb3860e0cb855eeeb47b4b87bd2477f54cbbb2
Ryan <yan1511@gmail.com>
2025-10-21 19:49:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/nobarecoffeshop/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.nobarecoffeshop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nobarecoffeshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
