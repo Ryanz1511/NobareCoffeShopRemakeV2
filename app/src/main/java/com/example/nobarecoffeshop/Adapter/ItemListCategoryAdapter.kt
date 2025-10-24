d92fb9b1ee25267fc2296ad7a48552e0d59f3368
Ryan <yan1511@gmail.com>
2025-10-24 08:16:24 +0700

    update price dollar ke idr dan update category food

--- app/src/main/java/com/example/nobarecoffeshop/Adapter/ItemListCategoryAdapter.kt
+++ app/src/main/java/com/example/nobarecoffeshop/Adapter/ItemListCategoryAdapter.kt
@@ -7,31 +7,37 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.nobarecoffeshop.Domain.ItemsModel
 import com.example.nobarecoffeshop.databinding.ViewholderItemListBinding
-import com.example.nobarecoffeshop.databinding.ViewholderPopularBinding
+import java.text.NumberFormat
+import java.util.Locale
 
+class ItemListCategoryAdapter(val items: MutableList<ItemsModel>) :
+    RecyclerView.Adapter<ItemListCategoryAdapter.Viewholder>() {
 
-class ItemListCategoryAdapter(val items: MutableList<ItemsModel>):
-    RecyclerView.Adapter<ItemListCategoryAdapter.Viewholder>(){
+    lateinit var context: Context
 
-        lateinit var context: Context
-
-    class Viewholder(val binding: ViewholderItemListBinding):
+    class Viewholder(val binding: ViewholderItemListBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ItemListCategoryAdapter.Viewholder {
-        context=parent.context
-        val binding=ViewholderItemListBinding.
-        inflate(LayoutInflater.from(context),parent,false)
+        context = parent.context
+        val binding = ViewholderItemListBinding.inflate(
+            LayoutInflater.from(context), parent, false
+        )
         return Viewholder(binding)
     }
 
     override fun onBindViewHolder(holder: ItemListCategoryAdapter.Viewholder, position: Int) {
-        holder.binding.titleTxt.text=items[position].title
-        holder.binding.priceTxt.text="$"+items[position].price.toString()
-        holder.binding.subtitleTxt.text=items[position].extra.toString()
+        holder.binding.titleTxt.text = items[position].title
+
+
+        val formattedPrice =
+            NumberFormat.getNumberInstance(Locale("id", "ID")).format(items[position].price.toInt())
+        holder.binding.priceTxt.text = "Rp$formattedPrice"
+
+        holder.binding.subtitleTxt.text = items[position].extra.toString()
 
         Glide.with(context)
             .load(items[position].picUrl[0])
@@ -42,6 +48,5 @@ class ItemListCategoryAdapter(val items: MutableList<ItemsModel>):
         }
     }
 
-    override fun getItemCount(): Int =items.size
-
+    override fun getItemCount(): Int = items.size
 }
